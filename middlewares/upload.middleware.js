ff66e8c28fd392c88e777dec118f1f7086c160ea
Shivam_Singh <shivamsingh6490@gmail.com>
2026-04-09 23:58:52 +0530

    added multiple files and crashed fixing

--- middlewares/upload.middleware.js
+++ middlewares/upload.middleware.js
@@ -1,14 +1,17 @@
 import multer from "multer"
 
-const storage =multer.diskStorage({
-    destination:function (req,file,cb){
-        cb(null,'upload');
-    },
-    filename:function (req,file,cb){
-        cb(null,file.originalname);
-    }
-})
 
-const upload=multer({storage});
+const upload=multer({
+    storage:multer.memoryStorage(),
+    limits:{
+        fileSize:5*1024*1024 //5MB
+    },
+    fileFilter:(req, file, cb)=>{
+        if(!file.mimetype.startsWith("image/")){
+            return cb(new Error("Only image files are allowed"),false);
+        }
+        cb(null,true);
+    }
+});
 
 export default upload
