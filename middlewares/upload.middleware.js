46d7814ec8962b33257a6f2242ed0978234be144
Shivam_Singh <shivamsingh6490@gmail.com>
2026-05-05 23:34:50 +0530

    relearning

--- middlewares/upload.middleware.js
+++ middlewares/upload.middleware.js
@@ -1,18 +1,21 @@
 import multer from "multer"
 
 
+// const upload=multer({
+//     storage:multer.memoryStorage(),
+//     limits:{
+//         fileSize:5*1024*1024 //5MB
+//     },
+//     fileFilter:(req, file, cb)=>{
+//         if(!file.mimetype.startsWith("image/")){
+//             return cb(new Error("Only image files are allowed"),false);
+//         }
+//         cb(null,true);
+//     }
+// });
+//
 const upload=multer({
     storage:multer.memoryStorage(),
-    limits:{
-        fileSize:5*1024*1024 //5MB
-    },
-    fileFilter:(req, file, cb)=>{
-        if(!file.mimetype.startsWith("image/")){
-            return cb(new Error("Only image files are allowed"),false);
-        }
-        cb(null,true);
-    }
-});
-//
+})
 
 export default upload
